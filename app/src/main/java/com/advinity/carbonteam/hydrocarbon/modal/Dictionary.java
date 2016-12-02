08ff46247fdfb809aa7f332c388a9f2c5905f7cc
afdolash <afdolash18@gmail.com>
2016-12-02 09:28:32 +0700

    versi alpha

--- app/src/main/java/com/advinity/carbonteam/hydrocarbon/modal/Dictionary.java
+++ app/src/main/java/com/advinity/carbonteam/hydrocarbon/modal/Dictionary.java
@@ -1,10 +1,12 @@
 package com.advinity.carbonteam.hydrocarbon.modal;
 
+import java.util.Comparator;
+
 /**
  * Created by afdolash on 25/11/16.
  */
 
-public class Dictionary {
+public class Dictionary implements Comparable {
     private String title, content;
 
     public Dictionary(String title, String content) {
@@ -26,5 +28,11 @@ public class Dictionary {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Dictionary dictionary = (Dictionary) o;
+        return this.getTitle().compareTo(((Dictionary) o).getTitle());
     }
 }
